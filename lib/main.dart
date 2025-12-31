9f9ac88297da46971e010d9b4f51eea0522bedae
dolbobob00 <dolbobob00@gmail.com>
2025-12-31 03:02:58 +0300

    feat: basic configurating of supabase, integrated dotenv, upd in required libs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'core/router/app_router.dart';
 import 'core/theme/theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  /// Load .env file
+  await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  /// Provide your own PUBLISHABLE_KEY and ANON_KEY in .env file
+  await Supabase.initialize(
+    url: dotenv.get('PUBLISHABLE_KEY'),
+    anonKey: dotenv.get('ANON_KEY'),
+  );
+
   runApp(const SophyApp());
 }
 
@@ -10,17 +24,13 @@ class SophyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'S O P H Y',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const Scaffold(
-        body: Center(
-          child: Text('Welcome to Sophy'),
-        ),
-      ),
+      routerConfig: router,
     );
   }
 }
